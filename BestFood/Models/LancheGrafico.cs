32ebf0f48fb54cb342a7a9fe792f66e86c362431
Pedro Henrique <ppphsantos@gmail.com>
2023-01-30 05:01:36 -0300

    Implementacao dos graficos de vendas

---
+++ BestFood/Models/LancheGrafico.cs
@@ -0,0 +1,10 @@
+﻿namespace BestFood.Models
+{
+    public class LancheGrafico
+    {
+        public string LancheNome { get; set; }
+        public int LanchesQuantidade { get; set; }
+        public decimal LanchesValorTotal { get; set; }
+
+    }
+}
